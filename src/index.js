5a09e7bd5f96f345259c4dd997b18037d9b0764c
kit-kiten <kit.kiten01@gmail.com>
2023-01-07 11:55:36 +1000

    feat: realised task in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let stringReverseN = n.toString();
+
+  if (stringReverseN[0] == '-'){
+    stringReverseN = stringReverseN.slice(1);
+  }
+
+  stringReverseN = stringReverseN.split('').reverse().join('');
+
+  return Number(stringReverseN);
 }
